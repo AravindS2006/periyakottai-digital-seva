7c28064fca46280031ce4d86095e16a58c9c4e5e
Aravindselvan <150777603+AravindS2006@users.noreply.github.com>
2026-09-08 10:39:07 +0530

    Update villageInfoData.ts

--- src/data/villageInfoData.ts
+++ src/data/villageInfoData.ts
@@ -34,7 +34,7 @@ export const VILLAGE_INFO = {
   },
   hamlets: [
     { ta: 'பெரியாக்கோட்டை', en: 'Periyakottai' },
-    { ta: 'கருங்கல்பட்டி', en: 'Karungalpatti' },
+    { ta: 'பெரியகோட்டை மேற்குத் தெரு', en: 'Periyakottai West Street' },
     { ta: 'கந்தப்ப கவுண்டன் வலசு', en: 'Kandhappa Goundan Valasu' },
     { ta: '19 புதூர்', en: '19 Pudur' },
     { ta: 'மற்றவை (Others)', en: 'Others' }
